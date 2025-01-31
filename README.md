fdbe18fb9962b06bd46e631fd293888ba830bb0c
yousra_hamdan <youyousstahm26@gmail.com>
2025-01-31 12:30:13 +0100

    Initialazing my first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨
+
+Pizza Bliss – The Ultimate Pizza Ordering Experience 🍕✨
+Overview:
+Pizza Bliss is a sleek and user-friendly online pizza ordering platform designed for a smooth and enjoyable experience. With a simple and visually appealing interface, customers can easily customize and order their favorite pizzas in just a few clicks.
+
+🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨
+
+Key Features & Interface Design
+🔹 Home Page:
+
+A welcoming, vibrant landing page with high-quality images of delicious pizzas.
+A quick “menu” button to dive straight into home page to start ordering .
+and  Choose extra add-ons (Extra cheese, Garlic dip, Spicy drizzle).
+
+🔹 Order Summary & Checkout:
+
+A clear, interactive cart that updates in real-time.
+
+🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨🍕✨
+
+Design Aesthetic:
+🍕 Color Scheme: A warm palette of reds, yellows, and deep browns to reflect a fresh, baked feel.
+📱 Responsive Design: Fully optimized for mobile and desktop for seamless ordering.
+⚡ Fast & Smooth Navigation: Minimal clicks to complete an order for a hassle-free experience.
+
+💻💻💻💻💻tools used💻💻💻💻💻 :
+ReactJS – The core JavaScript library for building user interfaces.
+react-dom – Provides DOM-specific methods that enable React to interact with the web page.
+react-router-dom – A standard library for routing in React applications, allowing navigation between views.
+Redux – A state management library for JavaScript applications, ensuring predictable state across the app.
+react-redux – A binding library that helps React components interact with the Redux store.
+@reduxjs/toolkit – An official set of utilities that simplifies working with Redux, offering tools like createSlice and configureStore.
+redux-thunk – A middleware that allows action creators to return functions instead of plain objects for asynchronous operations.
+redux-logger – A middleware for logging Redux state changes, useful for debugging and development.
+CSS – The stylesheet language used to style your React components and overall app.
+
+⚠️⚠️⚠️⚠️⚠️⚠️ to run the repo run the following comands in ur terminal: ⚠️⚠️⚠️⚠️⚠️⚠️
+npm install 
+$env:NODE_OPTIONS="--openssl-legacy-provider"
+npm start
+
---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,89 @@
+import product_02_image_01 from "../images/product_2.1.jpg";
+import product_02_image_02 from "../images/product_2.2.jpg";
+import product_02_image_03 from "../images/product_2.3.jpg";
+
+import product_03_image_01 from "../images/product_3.1.jpg";
+import product_03_image_02 from "../images/product_3.2.jpg";
+import product_03_image_03 from "../images/product_3.3.jpg";
+
+import product_04_image_01 from "../images/product_4.1.jpg";
+import product_04_image_02 from "../images/product_4.2.jpg";
+import product_04_image_03 from "../images/product_4.3.png";
+
+const products = [
+
+
+  {
+    id: "01",
+    title: "Vegetarian Pizza",
+    price: 30.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "02",
+    title: "Double Cheese Margherita",
+    price: 45.0,
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    image03: product_03_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "03",
+    title: "Maxican Green Wave",
+    price: 80.0,
+    image01: product_04_image_01,
+    image02: product_04_image_02,
+    image03: product_04_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "04",
+    title: "Seafood Pizza",
+    price: 77.0,
+    image01: product_02_image_02,
+    image02: product_02_image_01,
+    image03: product_02_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "05",
+    title: "Thin Cheese Pizza",
+    price: 90.0,
+    image01: product_03_image_02,
+    image02: product_03_image_01,
+    image03: product_03_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "06",
+    title: "Pizza With Mushroom",
+    price: 110.0,
+    image01: product_04_image_02,
+    image02: product_04_image_01,
+    image03: product_04_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+];
+
+export default products;
