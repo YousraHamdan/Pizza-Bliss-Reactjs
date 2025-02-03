82a075e2a0a676ba87affe5d0f99b8bb580a7365
yousra_hamdan <youyousstahm26@gmail.com>
2025-02-03 09:00:59 +0100

    final touches

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ListGroup } from "reactstrap";
 
-import logo from "../../assets/images/res-logo.png";
+import logo from "../../assets/images/category-02.png";
 import "../../styles/footer.css";
 
 const Footer = () => {
@@ -9,18 +9,18 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__logo">
         <img src={logo} alt="logo" />
-        <h5>MyPizza</h5>
+        <h5>Pizza Bliss</h5>
         <p>Best Pizzas in Tangier, try it out!</p>
       </div>
       <div>
         <h5 className="footer__title mb-3">Delivery Time</h5>
         <ListGroup>
           <div className="delivery__time-item border-0 ps-0">
-            <span>Friday - Tuesday</span>
+            <span>Monday - Friday</span>
             <p>10:00am - 11:00pm</p>
           </div>
           <div className="delivery__time-item border-0 ps-0">
-            <span>Wednesday - Thursday</span>
+            <span>Saturday - Sunday</span>
             <p>Off day</p>
           </div>
         </ListGroup>
